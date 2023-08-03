ea6349a04ca4015875f6085cd00a66561c552b52
NaturalStupldity <olkhovatyi@knu.ua>
2023-08-03 03:47:39 +0300

    Code refactoring
    Moved functions for computing skeleton motion

--- sources/utils.py
+++ sources/utils.py
@@ -1,8 +1,10 @@
 import numpy as np
+import tensorflow as tf
 import scipy.ndimage.interpolation as inter
 
 from scipy.signal import medfilt
 from scipy.spatial.distance import cdist
+from keras.src.layers import Lambda, Reshape
 
 
 class Config:
@@ -10,10 +12,29 @@ class Config:
         self.frame_length = 32
         self.joints_number = 22
         self.joints_dimension = 3
-        self.classes_number = 14
         self.features_dimension = 231
+        self.classes_number = 14
         self.filters = 16
         self.data_directory = '../data/SHREC/'
+
+
+def poses_difference(frame):
+    H, W = frame.get_shape()[1], frame.get_shape()[2]
+    frame = tf.subtract(frame[:, 1:, ...], frame[:, :-1, ...])
+    frame = tf.image.resize(frame, size=[H, W])
+
+    return frame
+
+
+def pose_motion(frames, frame_length):
+    difference_slow = Lambda(lambda x: poses_difference(x))(frames)
+    difference_slow = Reshape((frame_length, -1))(difference_slow)
+
+    frames = Lambda(lambda x: x[:, ::2, ...])(frames)
+    difference_fast = Lambda(lambda x: poses_difference(x))(frames)
+    difference_fast = Reshape((int(frame_length / 2), -1))(difference_fast)
+
+    return difference_slow, difference_fast
 
 
 def zoom_frames(frames, new_frame_length=64, new_joints_number=25, new_joints_dimension=3):
